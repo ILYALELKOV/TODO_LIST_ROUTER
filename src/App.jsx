fe7826484398c48cb9309142ea79f3dc6dbd0507
Ilya Lelkov <ilya.lelkov@mail.ru>
2023-10-16 15:49:40 +1000

    Task 1 completed!!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from 'react'
+
 export const App = () => {
+	const [todos, setTodos] = useState([])
+	const TODO_URL = 'https://jsonplaceholder.typicode.com/todos'
+
+	useEffect(() => {
+		fetch(TODO_URL)
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error('Network error')
+				}
+				return response.json()
+			})
+			.then((data) => setTodos(data))
+			.catch((error) => console.warn(error))
+	}, [])
+
 	return (
-		<>
-			<h1>TEST</h1>
-		</>
+		<div className="container">
+			<h1>TODOS LIST</h1>
+			{todos.map((todo) => (
+				<div key={todo.id} className="todo_container">
+					<p>{todo.title}</p>
+				</div>
+			))}
+		</div>
 	)
 }
